41ed802b4665beaef9c2b77f0c079c2d3cfe508a
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-02-09 14:29:43 +1100

    stimulus controller sounds-new connected

---
+++ app/javascript/controllers/sounds_new_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  connect() {
+    console.log("Hello from sounds_new_controller Stimulus controller")
+  }
+}
