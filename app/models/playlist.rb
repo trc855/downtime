e8a4d3e92bdff23d6c1011c178e877c2ea1d534b
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-02-14 23:50:44 +1100

    Playlist destroy and validations

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,8 +1,8 @@
 class Playlist < ApplicationRecord
   belongs_to :user
-  has_many :playlist_sounds
+  has_many :playlist_sounds, dependent: :destroy
   has_many :sounds, through: :playlist_sounds
 
   validates :title, presence: true
-  # validates :user, presence: true
+  validates :user, presence: true
 end
--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,11 +1,10 @@
 class Sound < ApplicationRecord
   belongs_to :user
-  has_many :playlist_sounds
+  has_many :playlist_sounds, dependent: :destroy
 
   has_one_attached :audio, dependent: :destroy
 
   validates :audio, presence: true
-
   validates :location, presence: true
 
   geocoded_by :location
