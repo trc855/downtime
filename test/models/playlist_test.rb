00ae27ffcafe0086f887077a85dbb4472888b486
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-05-15 21:15:36 +1000

    basic playlist tests implemented

--- test/models/playlist_test.rb
+++ test/models/playlist_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class PlaylistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  User.create(email: "test@example.com", password: "password123", password_confirmation: "password123")
+
+  test "should not save without a valid user" do
+    playlist = Playlist.new(title: "test playlist")
+    assert_not playlist.save, "save without valid user"
+  end
+
+  test "should not save playlist without title" do
+    playlist = Playlist.new(user_id: 1)
+    assert_not playlist.save, "saved the playlist with no title"
+  end
+
+  test "should save with valid user and title" do
+    playlist = Playlist.new(user_id: 1, title: "test playlist")
+    assert playlist.save
+  end
 end
