ca1e7ff5d83c1f359ca41bad6ef4a84a4e3f8876
Thomas Cochrane <thomasrcochrane@icloud.com>
2023-05-16 19:22:44 +1000

    playlist - unique title tests

--- test/models/playlist_test.rb
+++ test/models/playlist_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class PlaylistTest < ActiveSupport::TestCase
-  User.create(email: "test@example.com", password: "password123", password_confirmation: "password123")
+  user = User.create(email: "test@example.com", password: "password123", password_confirmation: "password123")
 
   test "should not save without a valid user" do
     playlist = Playlist.new(title: "test playlist")
@@ -16,5 +16,22 @@ class PlaylistTest < ActiveSupport::TestCase
   test "should save with valid user and title" do
     playlist = Playlist.new(user_id: 1, title: "test playlist")
     assert playlist.save
+  end
+
+  test "title should be unique wrt user" do
+    Playlist.create(user_id: 1, title: "test playlist")
+
+    playlist = Playlist.new(user_id: 1, title: "test playlist")
+    assert_not playlist.save, "saved playlist with duplicate title"
+  end
+
+  test "title can be non unique globally" do
+    Playlist.create(user_id: 1, title: "test playlist")
+
+    new_user = User.create(email: "test2@example.com", password: "password123", password_confirmation: "password123")
+
+    playlist = Playlist.new(user_id: new_user.id, title: "test playlist")
+
+    assert playlist.save, "could not save playlist with globally non unique title"
   end
 end
